318ae0859ff52aa18eb5a61d76676edcf8a10388
rasesh <raseshrout@gmail.com>
2024-03-30 09:43:43 +0530

    changing matching logic for edge cases

--- client/src/components/WordGridRow.jsx
+++ client/src/components/WordGridRow.jsx
@@ -1,36 +1,23 @@
 import { useAppContext } from "../hooks/useAppContext";
 
-function getCounter(word){
-    let counter = new Map()
-    word.split("").map((letter) => {
-        if (!counter.has(letter)) {
-            counter[letter] = 0;
-        }
-        counter[letter]++;
-    });
-    return counter;
-}
-
 function WordGridRow({row, word, currentRow}) {
 
     const {wordGrid} = useAppContext();
     let bgClass = new Array(5).fill("");
     if (row < currentRow && word) {
-        console.log("Checking", wordGrid, word);
-        let wordCounter = getCounter(word);
-        let guessCounter = new Map();
-        wordGrid[row].map((letter, index)=>{
-            guessCounter[letter] = 0;
-            guessCounter[word[index]] = 0;
-        });
+        let remaining = word;
         for (let i = 0; i < 5; i++) {
-            guessCounter[wordGrid[row][i]] += 1;
             if (wordGrid[row][i] === word[i]) {
+                remaining = remaining.replace(wordGrid[row][i], '');
                 bgClass[i] = "word-letter-correct-place flipped";
-            } else if (guessCounter[wordGrid[row][i]] <= wordCounter[wordGrid[row][i]]) {
-                bgClass[i] = "word-letter-wrong-place flipped";
             } else {
                 bgClass[i] = "word-letter-not-present flipped";
+            }
+        }
+        for (let i = 0; i < 5; i++) {
+            if (wordGrid[row][i] !== word[i] && remaining.includes(wordGrid[row][i])) {
+                remaining = remaining.replace(wordGrid[row][i], '');
+                bgClass[i] = "word-letter-wrong-place flipped";
             }
         }
     }
